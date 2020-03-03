d1980c056d5b6027af4b2fb136a2178812f2b3d2
Fontinalis <barnabas.vac@gmail.com>
2020-03-03 12:14:21 +0100

    add list to gql

--- gql/type.go
+++ gql/type.go
@@ -7,3 +7,35 @@ type Type interface {
 	GetDescription() string
 	GetKind() schema.TypeKind
 }
+
+type List struct {
+	Name        string
+	Description string
+	Wrapped     Type
+}
+
+func NewList(t Type) Type {
+	return &List{
+		Name:        "List",
+		Description: "Built-in 'List' type",
+		Wrapped:     t,
+	}
+}
+
+func (l *List) GetName() string {
+	return l.Name
+}
+
+func (l *List) GetDescription() string {
+	return l.Description
+}
+
+func (l *List) GetKind() schema.TypeKind {
+	return schema.ListKind
+}
+
+func (l *List) Unwrap() schema.Type {
+	return l.Wrapped
+}
+
+var _ schema.List = &List{}
