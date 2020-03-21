b002deae33c17fd68e2f52f52aece6a56a8dfc4b
Fontinalis <barnabas.vac@gmail.com>
2020-03-21 13:14:01 +0100

    Add NonNull type

--- gql/type.go
+++ gql/type.go
@@ -39,3 +39,35 @@ func (l *List) Unwrap() schema.Type {
 }
 
 var _ schema.List = &List{}
+
+type NonNull struct {
+	Name        string
+	Description string
+	Wrapped     Type
+}
+
+func NewNonNull(t Type) Type {
+	return &NonNull{
+		Name:        "NonNull",
+		Description: "Built-in 'NonNull' type",
+		Wrapped:     t,
+	}
+}
+
+func (l *NonNull) GetName() string {
+	return l.Name
+}
+
+func (l *NonNull) GetDescription() string {
+	return l.Description
+}
+
+func (l *NonNull) GetKind() schema.TypeKind {
+	return schema.NonNullKind
+}
+
+func (l *NonNull) Unwrap() schema.Type {
+	return l.Wrapped
+}
+
+var _ schema.NonNull = &NonNull{}
