30bf50c2a1c45f09ee494614e72290a7ec9bc6f2
Fontinalis <barnabas.vac@gmail.com>
2020-03-22 17:47:49 +0100

    add InputObject and InputField to gql

--- pkg/gql/argument.go
+++ pkg/gql/argument.go
@@ -34,3 +34,69 @@ func (a *Argument) GetDefaultValue() interface{} {
 func (a *Argument) IsDefaultValueSet() bool {
 	return reflect.ValueOf(a.DefaultValue).IsValid()
 }
+
+type InputObject struct {
+	Description string
+	Name        string
+	Directives  Directives
+	Fields      InputFields
+}
+
+type InputField struct {
+	Description  string
+	Name         string
+	Type         Type
+	DefaultValue interface{}
+	Directives   Directives
+}
+
+type InputFields []schema.InputField
+
+func (o *InputObject) GetDescription() string {
+	return o.Description
+}
+
+func (o *InputObject) GetName() string {
+	return o.Name
+}
+
+func (o *InputObject) GetKind() schema.TypeKind {
+	return schema.ObjectKind
+}
+
+func (o *InputObject) GetDirectives() []schema.Directive {
+	return o.Directives
+}
+
+func (o *InputObject) GetFields() []schema.InputField {
+	return o.Fields
+}
+
+func (o *InputField) GetDescription() string {
+	return o.Description
+}
+
+func (o *InputField) GetName() string {
+	return o.Name
+}
+
+func (o *InputField) GetType() schema.Type {
+	return o.Type
+}
+
+func (o *InputField) GetDefaultValue() interface{} {
+	return o.DefaultValue
+}
+
+func (o *InputField) IsDefaultValueSet() bool {
+	return reflect.ValueOf(o.DefaultValue).IsValid()
+}
+
+func (o *InputField) GetDirectives() []schema.Directive {
+	return o.Directives
+}
+
+var (
+	_ schema.InputObjectType = &InputObject{}
+	_ schema.InputField      = &InputField{}
+)
