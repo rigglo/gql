84e4c1bc9c31031f264d815a4784b8fab7ca8df9
Fontinalis <barnabas.vac@gmail.com>
2020-03-22 14:05:23 +0100

    add Int scalar value coercion

--- pkg/gql/scalars.go
+++ pkg/gql/scalars.go
@@ -1,7 +1,10 @@
 package gql
 
 import (
+	"errors"
 	"fmt"
+	"math"
+	"strconv"
 	"strings"
 
 	"github.com/rigglo/gql/pkg/schema"
@@ -60,10 +63,95 @@ var Int *Scalar = &Scalar{
 	Name:        "Int",
 	Description: "This is the built-in 'Int' scalar type",
 	CoerceResultFunc: func(i interface{}) (interface{}, error) {
+		switch i.(type) {
+		case float32:
+			{
+				if i == float32(int(i.(float32))) {
+					return int(i.(float32)), nil
+				}
+				return nil, errors.New("Data loss, expected Int, got float32")
+			}
+		case float64:
+			{
+				if i == float64(int(i.(float64))) {
+					return int(i.(float64)), nil
+				}
+				return nil, errors.New("Data loss, expected Int, got float64")
+			}
+		case string:
+			{
+				if n, err := strconv.ParseInt(i.(string), 10, 32); err == nil {
+					return int(n), nil
+				}
+				return nil, errors.New("Data loss, expected Int, got string")
+			}
+		case bool:
+			{
+				if i.(bool) {
+					return 1, nil
+				}
+				return 0, nil
+			}
+		case int:
+			{
+				return i.(int), nil
+			}
+		case int8:
+			{
+				return int(i.(int8)), nil
+			}
+		case int16:
+			{
+				return int(i.(int16)), nil
+			}
+		case int32:
+			{
+				return int(i.(int32)), nil
+			}
+		case int64:
+			{
+				if i.(int64) > math.MaxInt32 || i.(int64) < math.MinInt32 {
+					return nil, errors.New("Data loss, expected Int (32bit), got int64")
+				}
+				return int(i.(int64)), nil
+			}
+		case uint:
+			{
+				if i.(uint) > math.MaxInt32 {
+					return nil, errors.New("Data loss, expected Int (32bit), got uint")
+				}
+				return int(i.(uint)), nil
+			}
+		case uint8:
+			{
+				return int(i.(uint8)), nil
+			}
+		case uint16:
+			{
+				return int(i.(uint16)), nil
+			}
+		case uint32:
+			{
+				if i.(uint32) > math.MaxInt32 {
+					return nil, errors.New("Data loss, expected Int (32bit), got uint32")
+				}
+				return int(i.(int64)), nil
+			}
+		case uint64:
+			{
+				if i.(uint64) > math.MaxInt32 {
+					return nil, errors.New("Data loss, expected Int (32bit), got uint64")
+				}
+				return int(i.(uint64)), nil
+			}
+		}
 		return i, nil
 	},
 	CoerceInputFunc: func(bs []byte) (interface{}, error) {
-		return string(bs), nil
+		if n, err := strconv.ParseInt(string(bs), 10, 32); err == nil {
+			return int(n), nil
+		}
+		return errors.New("Invalid Input, could not coerce value to Int"), nil
 	},
 }
 
