856c061b81350d74ce3eaf733f45a3f9d597a355
Fontinalis <barnabas.vac@gmail.com>
2020-03-26 20:01:53 +0100

    add basic handler

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,19 +3,27 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"html"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/rigglo/gql/pkg/gql"
 )
 
 type Config struct {
-	schema *gql.Schema
+	Schema *gql.Schema
 }
 
 func New(c Config) http.Handler {
 	return &handler{
 		conf: c,
 	}
+}
+
+type postParams struct {
+	Query         string          `json:"query"`
+	Variables     json.RawMessage `json:"variables"`
+	OperationName string          `json:"operationName"`
 }
 
 type handler struct {
@@ -33,14 +41,35 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				OperationName: r.URL.Query().Get("operationName"),
 			}
 		}
+	case http.MethodPost:
+		{
+			bs, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				break
+			}
+
+			p := postParams{}
+
+			err = json.Unmarshal(bs, &p)
+			if err != nil {
+				break
+			}
+
+			params = &gql.ExecuteParams{
+				Query:         html.UnescapeString(p.Query),
+				Variables:     string(p.Variables),
+				OperationName: p.OperationName,
+			}
+		}
 	}
 	if params != nil {
-		bs, err := json.MarshalIndent(gql.Execute(r.Context(), h.conf.schema, *params), "", "\t")
+		bs, err := json.MarshalIndent(gql.Execute(r.Context(), h.conf.Schema, *params), "", "\t")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		fmt.Fprint(w, bs)
+		fmt.Fprint(w, string(bs))
+		return
 	}
 	http.Error(w, "invalid query parameters", http.StatusBadRequest)
 }
