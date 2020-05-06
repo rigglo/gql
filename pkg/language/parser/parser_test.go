6f8d7d8f9b25fbd1d51506271edc9f81f91296cd
Fontinalis <barnabas.vac@gmail.com>
2020-05-06 21:14:43 +0200

    add scalar and object parsing

--- pkg/language/parser/parser_test.go
+++ pkg/language/parser/parser_test.go
@@ -1,7 +1,10 @@
 package parser_test
 
 import (
+	"log"
 	"testing"
+
+	"github.com/rigglo/gql/pkg/language/ast"
 
 	"github.com/rigglo/gql/pkg/language/parser"
 )
@@ -24,6 +27,46 @@ query {
 		t.Errorf("error: %v, at Line: %v, Col: %v", err, token.Line, token.Col)
 		return
 	}
+	// spew.Dump(doc)
+
+}
+
+func TestParseScalar(t *testing.T) {
+	query := `
+	"""some scalar""" scalar foo @depricated(reason: "just")
+	scalar bar`
+	token, doc, err := parser.Parse([]byte(query))
+	if err != nil {
+		t.Errorf("error: %v, at Line: %v, Col: %v", err, token.Line, token.Col)
+		return
+	}
+	log.Printf("%#v", doc.Definitions[0])
+	log.Printf("%#v", doc.Definitions[1])
+	// spew.Dump(doc)
+
+}
+
+func TestParseObject(t *testing.T) {
+	query := `
+	type Person {
+		name(
+			"some example arg"
+			bar: String
+
+			"some other arg"
+			foo: Int
+		): String
+		age: Int
+		picture: Url
+	}
+	`
+	token, doc, err := parser.Parse([]byte(query))
+	if err != nil {
+		t.Errorf("error: %v, at Line: %v, Col: %v", err, token.Line, token.Col)
+		return
+	}
+	log.Printf("%#v", doc.Definitions[0].(*ast.ObjectDefinition).Fields[0].Arguments[0])
+	//log.Printf("%#v", doc.Definitions[1])
 	// spew.Dump(doc)
 
 }
